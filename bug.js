ff10f47fff0dd9a82796b515fec93bc308b6afdd
BugHunter-X <askzenai@gmail.com>
2025-01-31 07:30:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <p>This is a paragraph of text.</p>
+    </div>
+  );
+}
+```
+This simple Next.js app produces a runtime error in Next.js 15 due to incorrect usage of the `app` directory structure.
